409df474b3479ed1c6e7bd3d605e28473c6e6b28
Parsa <mo.rostami@digikala.com>
2021-08-11 00:19:58 +0430

    minor changes.

---
+++ migrate.sh
@@ -0,0 +1,3 @@
+python3 manage.py makemigrations problem_solving human_resources
+python3 manage.py makemigrations
+python3 manage.py migrate
---
+++ start.sh
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+./redis.sh
+python3 manage.py runserver
