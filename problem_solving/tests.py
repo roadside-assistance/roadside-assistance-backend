c48a2151bdd9d4619bf5a275026a6d8060933b26
Parsa Salavati <parsa.salavati@gmail.com>
2021-09-05 00:37:47 +0430

    Add unit tests for problem class. Specifically when creating a new one and approving one.

--- problem_solving/tests.py
+++ problem_solving/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from human_resources.inspector.model import InspectorModel
+from human_resources.team.model import TeamModel
+from human_resources.worker.model import WorkerModel
+from problem_solving.problem.model import ProblemModel
+
+
+class ProblemModelTests(TestCase):
+
+    def test_assign_to_inspector_after_submit(self):
+        """A new task is assigned correctly to an inspector"""
+        problem = ProblemModel(status='NEW')
+        inspector = InspectorModel(town_under_control='TEHRAN')
+        problem.save()
+        self.assertEqual(inspector, problem.assigned_to_inspector)
+
+    def test_assign_assign_to_team(self):
+        """Task is assigned to a team when approved"""
+        inspector = InspectorModel(town_under_control='TEHRAN')
+        worker = WorkerModel()
+        team = TeamModel(leader=worker)
+        problem = ProblemModel(status='APPROVED', assigned_to_inspector=inspector, number_of_needed_workers=1)
+        problem.save()
+        self.assertEqual(problem.assigned_to_team, team)
